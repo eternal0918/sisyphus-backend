2c67b7e031751067b946ea997b9d30514a5c4b2b
eternal0918 <eternal0918@163.com>
2025-01-23 12:34:10 +0800

    项目初始化

--- sql/ry_config_20240914.sql
+++ sql/ry_config_20240914.sql
@@ -1,11 +1,11 @@
-DROP DATABASE IF EXISTS `ry-config`;
+DROP DATABASE IF EXISTS `nacos_232`;
 
-CREATE DATABASE  `ry-config` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE  `nacos_232` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-USE `ry-config`;
+USE `nacos_232`;
 
 /******************************************/
 /*   表名称 = config_info   */
